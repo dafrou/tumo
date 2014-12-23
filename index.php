72126df4ded2d5ff677b7f3add884c19fe71609f
Christian Meza <christianmezah11@gmail.com>
2014-12-23 11:54:49 -0300

    rematando para subida

--- index.php
+++ index.php
@@ -581,15 +581,10 @@
                 <div class="row animated opacity mar-bot0" data-andown="fadeIn" data-animation="animation">
                     <div class="col-sm-12 align-center">
                         <ul class="social-network social-circle">
-                            <li><a href="#" class="icoRss" title="Rss"><i class="fa fa-rss"></i></a></li>
-                            <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li>
-                            <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+                            <li><img src="img/logoTumu.png" height="350" width="350"></img></li>
                         </ul>				
                     </div>
                 </div>
-
                 <div class="row align-center copyright">
                     <div class="col-sm-12"><p>Copyright &copy; TUMU</p></div>
                 </div>
